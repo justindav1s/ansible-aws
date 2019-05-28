489b8b237e469f161b9e897e512ecb313fa21ec2
Justin Davis <justinndavis@gmail.com>
2019-05-28 11:03:27 +0100

    eks setup stuff

--- bin/authenticate_workers.sh
+++ bin/authenticate_workers.sh
@@ -2,8 +2,31 @@
 
 echo KUBECONFIG = $KUBECONFIG
 
-# substiute nodegroup arn into ../kube/aws-auth-cm.yaml
-# are produced by cloudformation process, arn looks like this
+# substitute nodegroup arn into ../kube/aws-auth-cm.yaml
+# arn is produced by cloudformation process, arn looks like this
 # arn:aws:iam::************:role/eks-worker-nodes1-1-NodeInstanceRole-XGUWNN6Y2E3
+
+STACKNAME="eks-worker-nodes1-1"
+
+ARN=$(aws cloudformation describe-stacks \
+    | jq -r --arg STACKNAME "$STACKNAME" \
+    '.Stacks[] | select(.StackName==$STACKNAME).Outputs[] | select(.OutputKey=="NodeInstanceRole").OutputValue')
+
+echo ARN : $ARN
+
+cat <<EOF > ../kube/aws-auth-cm.yaml
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: aws-auth
+  namespace: kube-system
+data:
+  mapRoles: |
+    - rolearn: ${ARN}
+      username: system:node:{{EC2PrivateDNSName}}
+      groups:
+        - system:bootstrappers
+        - system:nodes
+EOF
 
 kubectl apply -f ../kube/aws-auth-cm.yaml
